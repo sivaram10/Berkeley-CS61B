0bc684a98ea9e02115cab6ad080ad5463f843a68
sivaram10 <kottapallisivaram@gmail.com>
2019-05-29 20:01:23 -0400

    Completed lab2

--- lab2/Intlist/IntList.java
+++ lab2/Intlist/IntList.java
@@ -82,7 +82,20 @@ public class IntList {
 
     public static IntList dcatenate(IntList A, IntList B) {
         //TODO:  fill in method
-        return null;
+        if (A == null) {
+            return null;
+        }
+        else {
+            IntList res = A;
+            while (res != null) {
+                res = res.rest;
+                if (res.rest == null) {
+                    res.rest = B;
+                    return A;
+                }
+            }
+            return A;
+        }
     }
 
     /**
@@ -91,21 +104,24 @@ public class IntList {
      */
     public static IntList catenate(IntList A, IntList B) {
         //TODO:  fill in method
-        return null;
+        if (A == null) {
+            return null;
+        }
+        IntList res = new IntList(A.first, null);
+        IntList ptr = res;
+        A = A.rest;
+        while (A != null) {
+            ptr.rest = new IntList(A.first, null);
+            A = A.rest;
+            ptr = ptr.rest;
+        }
+        while (B != null) {
+            ptr.rest = new IntList(B.first, null);
+            B = B.rest;
+            ptr = ptr.rest;
+        }
+        return res;
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
